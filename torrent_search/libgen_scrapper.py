e854c15d606ea1967a0633bfed51e54c043a158c
Emir Akdag <emirakdag@protonmail.com>
2023-10-10 04:18:03 +0300

    add function which extract data from libgen search table.

--- torrent_search/libgen_scrapper.py
+++ torrent_search/libgen_scrapper.py
@@ -20,6 +20,29 @@ def get_libgen_search(query, proxies):
         raise Exception('Search results not found on page.')
     return table
 
+def extract_libgen_data(table):
     
+    # Initialize an empty list to store the data
+    data = []
 
-    
+    # Loop through the rows of the table
+    for row in table.find_all('tr'):
+        # Initialize an empty dictionary for each row
+        row_data = {}
+
+        # Extract data from each cell in the row
+        cells = row.find_all('td')
+        if len(cells) >= 11:
+            row_data['ID'] = cells[0].text.strip()
+            row_data['Authors'] = cells[1].text.strip()
+            row_data['Title'] = cells[2].text.strip()
+            row_data['Publisher'] = cells[3].text.strip()
+            row_data['Year'] = cells[4].text.strip()
+            row_data['Pages'] = cells[5].text.strip()
+            row_data['Language'] = cells[6].text.strip()
+            row_data['Size'] = cells[7].text.strip()
+            row_data['Extension'] = cells[8].text.strip()
+
+            # Append  row data to the list
+            data.append(row_data)
+    return data
